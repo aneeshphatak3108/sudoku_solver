26ad2f6f5bc9d3ae4e067081b0ba867703034abf
Shreeyash Patil <shreeyashpatil313@gmail.com>
2024-11-16 12:07:46 +0530

    Delete mixed.h

--- mixed.h
+++
@@ -1,6 +0,0 @@
-#define N 9
-#define EMPTY 0
-
-void take_input(stack *u, stack *r, int sudoku[N][N], int skeleton[N][N], int row, int col, int num);
-void undo(stack *u, stack *r, int sudoku[N][N]);
-void redo(stack *u, stack *r, int sudoku[N][N]);
